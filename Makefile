2494790bd869f1b14216e79eb5bb14f45e894034
ghollisjr <ghollisjr@gmail.com>
2012-09-21 18:50:43 -0400

    Now it's got a shared library libOptions.so, make sure to put that where the system will know where to find it, I've got mine setup to use ~/lib.

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-demo : demo.cc
-	g++ demo.cc -o demo
+demo : demo.cc libOptions.so
+	g++ -L"." -lOptions demo.cc -o demo
+libOptions.so : Options.hh Options.cc
+	g++ -shared -fPIC Options.cc -o libOptions.so
 clean:
-	rm -f demo
+	rm -f demo libOptions.so
--- Options.hh
+++ Options.hh
@@ -101,49 +101,7 @@ public:
   std::string GetErrorString() {return e;}
 };
 
-inline void parseargs(int argc, char** argv,
-		      OptionSpec& options, Flags& flags,
-		      std::vector<std::string>& args)
-{
-  char optchar = 0;
-  const int numopts = options.size();
-  option longopts[numopts];
-
-  std::string shortoptsstr = options.GetShortOpts();
-
-  char shortopts[shortoptsstr.size()+1];
-  for(int i = 0; i < shortoptsstr.size(); ++i) {
-    shortopts[i] = shortoptsstr[i];
-  }
-  for(int i = 0; i < numopts; ++i) longopts[i].flag=0;
-  int index=0;
-  for(OptionSpec::optionspeciter i = options.options.begin();
-      i != options.options.end();
-      i++) {
-    longopts[index].name = i->second.longopt.c_str();
-    longopts[index].val = i->second.shortopt;
-    longopts[index].has_arg = i->second.argspec;
-    index++;
-  }
-  while( (optchar = getopt_long(argc,argv,shortopts,longopts,0))
-	 != -1 && optchar != '?') {
-    Flag currentflag = options.options[optchar];
-    int tmp = options.options[optchar].argspec;
-    if(tmp != no_argument) {
-      Flag tmpflag = currentflag;
-      tmpflag.arg = optarg == 0 ? "" : optarg;
-      flags.flags.insert(std::make_pair(optchar,tmpflag));
-    }
-    else
-      flags.flags.insert(std::make_pair(optchar,currentflag));
-  }
-  if(optchar == '?')
-    throw ParseException("Error"); //I'm not sure what it means at the moment
-  int j = 0;
-  args.resize(argc-optind);
-  for(int i = optind; i < argc; (++i, ++j)) {
-    args[j] = argv[i];
-  }
-  return;
-}
+void parseargs(int argc, char** argv,
+	       OptionSpec& options, Flags& flags,
+	       std::vector<std::string>& args);
 #endif
